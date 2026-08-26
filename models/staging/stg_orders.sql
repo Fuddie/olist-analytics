f285406f38924a75cb045d0e06540833fc50de72
Fuddie <58942947+Fuddie@users.noreply.github.com>
2026-08-26 14:13:48 +0100

    Propagate order ingestion metadata

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,16 +1,17 @@
 with source as (
 
     select
-      order_id,
-      customer_id,
-      order_status,
-      order_purchase_timestamp,
-      order_approved_at,
-      order_delivered_carrier_date,
-      order_delivered_customer_date,
-      order_estimated_delivery_date,
-      from 
-     {{ source('olist_raw', 'orders') }}
+        order_id,
+        customer_id,
+        order_status,
+        order_purchase_timestamp,
+        order_approved_at,
+        order_delivered_carrier_date,
+        order_delivered_customer_date,
+        order_estimated_delivery_date,
+        _loaded_at
+    from {{ source('olist_raw', 'orders') }}
+
 ),
 
 renamed as (
@@ -23,20 +24,20 @@ renamed as (
         order_approved_at,
         order_delivered_carrier_date,
         order_delivered_customer_date,
-        order_estimated_delivery_date
-
+        order_estimated_delivery_date,
+        _loaded_at
     from source
+
 )
 
 select
-  order_id,
-  customer_id,
-  order_status,
- order_purchase_timestamp,
- order_approved_at,
- order_delivered_carrier_date,
- order_delivered_customer_date,
- order_estimated_delivery_date
-   
+    order_id,
+    customer_id,
+    order_status,
+    order_purchase_timestamp,
+    order_approved_at,
+    order_delivered_carrier_date,
+    order_delivered_customer_date,
+    order_estimated_delivery_date,
+    _loaded_at
 from renamed
-
