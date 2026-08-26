0da0778f168ac057eec168c826e2246e7dcec2f8
Fuddie <58942947+Fuddie@users.noreply.github.com>
2026-08-26 14:14:22 +0100

    Propagate review ingestion metadata

--- models/staging/stg_order_reviews.sql
+++ models/staging/stg_order_reviews.sql
@@ -1,26 +1,31 @@
 with source as (
-        select
+
+    select
         review_id,
         order_id,
         review_score,
         review_comment_title,
         review_comment_message,
         review_creation_date,
-        review_answer_timestamp
+        review_answer_timestamp,
+        _loaded_at
     from {{ source('olist_raw', 'order_reviews') }}
 
 ),
 
 cleaned as (
- select
+
+    select
         review_id,
         order_id,
         review_score,
         nullif(trim(review_comment_title), '') as review_comment_title,
         nullif(trim(review_comment_message), '') as review_comment_message,
         review_creation_date,
-        review_answer_timestamp
+        review_answer_timestamp,
+        _loaded_at
     from source
+
 )
 
 select
@@ -30,20 +35,6 @@ select
     review_comment_title,
     review_comment_message,
     review_creation_date,
-    review_answer_timestamp
+    review_answer_timestamp,
+    _loaded_at
 from cleaned
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
