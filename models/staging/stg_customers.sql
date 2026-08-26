b3e364326b20dcd792ff11b1852a3befb51a1a55
Fuddie <58942947+Fuddie@users.noreply.github.com>
2026-08-26 14:13:40 +0100

    Propagate ingestion metadata through staging

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,11 +1,12 @@
 with source as (
 
-    select 
-    customer_id,
+    select
+        customer_id,
         customer_unique_id,
         customer_zip_code_prefix,
         customer_city,
-        customer_state
+        customer_state,
+        _loaded_at
     from {{ source('olist_raw', 'customers') }}
 
 ),
@@ -17,16 +18,17 @@ renamed as (
         customer_unique_id,
         customer_zip_code_prefix,
         trim(customer_city) as customer_city,
-        upper(trim(customer_state)) as customer_state
-
+        upper(trim(customer_state)) as customer_state,
+        _loaded_at
     from source
 
 )
 
 select
- customer_id,
- customer_unique_id,
- customer_zip_code_prefix,
- customer_city,
- customer_state
+    customer_id,
+    customer_unique_id,
+    customer_zip_code_prefix,
+    customer_city,
+    customer_state,
+    _loaded_at
 from renamed
